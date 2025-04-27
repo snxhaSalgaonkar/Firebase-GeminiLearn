b663ae85f42427cada7233f8a019107848cb79f6
snxhaSalgaonkar <snehasalgaonkar.012@gmail.com>
2025-04-27 13:15:13 -0700

    GeminiPromptStream

--- firebasestart/lib/gemini/start3.dart
+++ firebasestart/lib/gemini/start3.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 
@@ -14,33 +15,49 @@ class _TextToListScreenState extends State<TextToListScreen> {
   String _response = '';
   String get response => _response;
   bool _isLoading = false;
+  String ques = '';
 
-  void _addItem(String text) {
+  void _addItem(String text, String resp) {
     if (text.trim().isEmpty) return;
     setState(() {
-      mapp.addAll({text: response});
-      //maplist.add({"Question": text, "Answer": response});
+      mapp.addAll({text: resp});
     });
-    _controller.clear();
-    _response = ''; // Clear the TextField
+  }
+
+  void adddata(String ques, String ans) async {
+    if (ques == '' || ans == '') {
+      print("************#########***************");
+      print('No data found');
+    } else {
+      FirebaseFirestore.instance
+          .collection("Gemini")
+          .doc()
+          .set({"Ques": ques, "Answer": ans})
+          .then((value) {
+            print("************");
+            print('data inserted');
+          });
+    }
+  }
+
+  void clear(TextEditingController controller) {
+    {
+      controller.clear();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Text to List Example"),
-        backgroundColor: Colors.blueAccent,
-      ),
+      appBar: AppBar(title: const Text("Gemini API"), centerTitle: true),
       body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
               controller: _controller,
-              //onSubmitted: _addItem, // Press Enter to add
               decoration: InputDecoration(
-                hintText: "Type something and press Enter",
+                hintText: "Ask Gemini",
                 filled: true,
                 fillColor: Colors.grey.shade100,
                 contentPadding: const EdgeInsets.symmetric(
@@ -64,24 +81,32 @@ class _TextToListScreenState extends State<TextToListScreen> {
             onPressed: () {
               setState(() {
                 _isLoading = true; // Show loading spinner
-                //_response = ''; // Clear old response
+              });
+
+              //to take a new response
+              setState(() {
+                _response = '';
+                ques =
+                    _controller.text.toString() +
+                    ". Give answer in maximun 4 Points";
               });
 
               Gemini.instance
-                  .promptStream(parts: [Part.text(_controller.text.toString())])
+                  .promptStream(parts: [Part.text(ques)])
                   .listen(
                     (value) {
                       _response += value?.output.toString() ?? '';
                       print("*****" + response);
-                    
                     },
                     onDone: () {
                       setState(() {
                         _isLoading = false;
                       });
                       print("*****************" + response);
-                      //_controller.clear();
-                      _addItem(_controller.text.toString());
+                      print("*****************#######" + _response);
+                      _addItem(ques, response);
+                      adddata(ques, response);
+                      clear(_controller);
                     },
                   );
             },
@@ -101,45 +126,6 @@ class _TextToListScreenState extends State<TextToListScreen> {
                 ),
               ),
           SizedBox(height: 20),
-
-          // ElevatedButton(
-          //   onPressed: () {
-          //     setState(() {
-          //       _isLoading = true; // Show loading spinner
-          //       _response = ''; // Clear old response
-          //     });
-
-          //     Gemini.instance
-          //         .promptStream(parts: [Part.text(_controller.text.toString())])
-          //         .listen(
-          //           (value) {
-          //             _response += value?.output.toString() ?? '';
-          //             print("*****" + response);
-          //             // _addItem(_controller.text.toString());
-          //           },
-          //           onDone: () {
-          //             setState(() {
-          //               _isLoading = false;
-          //             });
-          //             print("*****************" + response);
-          //             _controller.clear();
-          //           },
-          //         );
-          //   },
-          //   child: Text('ASK GEMINI'),
-          // ),
-          // Expanded(
-          //   child: ListView.builder(
-          //     itemCount: mapp.length,
-          //     //itemCount: _items.length,
-          //     itemBuilder: (context, index) {
-          //       String key = mapp.keys.elementAt(index);
-          //       String value = mapp.values.elementAt(index);
-          //       return ListTile(title: Text(key), subtitle: Text(value));
-
-          //     },
-          //   ),
-          // ),
         ],
       ),
     );
